9233e0b0ec3a961ecdb1c7e998da1638ca84b772
Sebastian Aage Nielsen <u33674@rts-365.dk>
2023-03-14 11:15:21 +0100

    navigation setup and welcome screen

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,15 @@ import useSessionStorage from './hooks/useSessionStorage'
 import Layout from './Layout'
 
 // Import Pages
+import Welcome from './pages/Welcome'
+import Activities from './pages/Activities'
+import ActivityDetails from './pages/ActivityDetails'
+import Calendar from './pages/Calendar'
+import Search from './pages/Search'
 
 // Import Contexts
 import AuthProvider from './contexts/AuthProvider'
-import Welcome from './pages/Welcome'
-import Activities from './pages/Activities'
+import TeamOverview from './pages/TeamOverview'
 
 function App() {
 	const [authCookie, setAuthCookie] = useCookie('auth')
@@ -23,12 +27,27 @@ function App() {
 
 	const router = createBrowserRouter([
 		{
-			path: '/',
 			element: showWelcomeScreen() ? <Welcome /> : <Layout />,
 			children: [
 				{
-					index: true,
+					path: '/',
 					element: <Activities />,
+				},
+				{
+					path: '/activity/:id',
+					element: <ActivityDetails />,
+				},
+				{
+					path: '/calendar',
+					element: <Calendar />,
+				},
+				{
+					path: '/search',
+					element: <Search />,
+				},
+				{
+					path: '/calendar/team/:id',
+					element: <TeamOverview />,
 				},
 			],
 		},
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,7 +1,20 @@
 export default function Welcome() {
 	return (
-		<div>
-			<h1>Welcome</h1>
+		<div
+			className='relative bg-[url("./splash-image.jpg")] bg-cover h-screen w-screen'
+			style={{ backgroundPosition: 'center' }}
+		>
+			<div className='absolute left-0 top-1/2 flex flex-col gap-2'>
+				<h1 className='flex flex-col pl-11 pr-4'>
+					<span className='font-display-header text-white/25 font-bold uppercase text-[38px] text-stroke-2 leading-none tracking-tighter'>
+						Landrup
+					</span>
+					<span className='block font-display text-[74px] text-[#E856EB] uppercase text-stroke-1 leading-none -mt-3'>
+						Dans
+					</span>
+				</h1>
+				<div className=' w-full h-3 bg-[#913693] shadow-[0_4px_4px_#00000040]' />
+			</div>
 		</div>
 	)
 }
